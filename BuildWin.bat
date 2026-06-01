e2c63c94202f0b4236fec6b11fb91febd1558fad
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2026-06-01 15:56:13 +0200

    Act 1

---
+++ BuildWin.bat
@@ -0,0 +1,3 @@
+Clear.bat
+python -m PyInstaller --onefile --windowed --noconsole --icon=fnaf-into-the-pit.ico NoCompatibleToInstall.py
+echo "v1.0.0.2b" > GameVersion.txt
---
+++ BuildWin.ps1
@@ -0,0 +1,3 @@
+./Clear.ps1
+python -m PyInstaller --onefile --windowed --noconsole --icon=fnaf-into-the-pit.ico NoCompatibleToInstall.py
+echo "v1.0.0.2b" > GameVersion.txt
